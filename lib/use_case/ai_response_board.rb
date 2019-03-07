379decb2383aff135d8d35601a0ee674bbb73886
csabagyorfi <csaba@madetech.com>
2019-03-07 14:12:04 +0000

    moved global constant to class

--- lib/use_case/ai_response_board.rb
+++ lib/use_case/ai_response_board.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class AIResponse
+  WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
+  
   def execute(board)
     if board[4].nil?
       4
