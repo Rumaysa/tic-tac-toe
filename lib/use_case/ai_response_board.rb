0cfa0c16cce6490f0e4b871cb249c548541593c5
Maysa <maysa@madetech.com>
2019-03-18 12:02:32 +0000

    feat:removing block opponent method

--- lib/use_case/ai_response_board.rb
+++ lib/use_case/ai_response_board.rb
@@ -6,13 +6,7 @@ class AIResponse
   end
 
   def execute(board)
-    if board[4].nil?
-      4
-    elsif board[4] == 'X'
-      0
-    else
-      block_opponent(board)
-    end
+    minimax(board)
   end
 
   private
@@ -29,7 +23,34 @@ class AIResponse
     end.first.first
   end
 
+  def minimax(board, player = 'O')
+    possible_moves = board.map.with_index { |move, i| i if cell.nil? }
+    possible_moves.each do | position |
+      board[position] = player
+      player = player == 'O' ? 'X' : 'O'
+    
+  end
+
+  def minimax_idea(board, player)
+    empty_cell_indexes = board.map.with_index { |cell, i| i if cell.nil? }
+    scored_moves = []
+    return scored_moves if board.evaluate == :game_over
+    empty_cell_indexes.each do |cell_index|
+      temp_board = board
+      temp_board[cell_index] = player
+      scored_moves << [10, cell_index] if temp_board.evaluate == :player_two_wins
+      scored_moves << [-10, cell_index] if temp_board.evaluate == :player_one_wins
+      scored_moves << [0, cell_index] if temp_board.evaluate == :game_over
+      scored_moves << if player == 'O'
+                        minimax(temp_board, 'X')
+                      else
+                        minimax(temp_board, 'O')
+                      end
+    end
+  end
+
   def already_blocked(combination, board)
     combination.any? { |i| board[i] == 'O' }
   end
 end
+end 
