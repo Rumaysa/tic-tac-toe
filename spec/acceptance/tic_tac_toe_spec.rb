4a1c7b8c5fe687fb30986eb206b984417ac9bba9
Maysa <maysa@madetech.com>
2019-03-21 10:00:14 +0000

    reomve clear board use case

--- spec/acceptance/tic_tac_toe_spec.rb
+++ spec/acceptance/tic_tac_toe_spec.rb
@@ -5,7 +5,6 @@ require 'gateway/game_gateway'
 require 'use_case/view_board'
 require 'use_case/update_board'
 require 'use_case/evaluate_board'
-require 'use_case/clear_board'
 require 'use_case/ai_response_board'
 require 'ui_board'
 require 'domain/game'
@@ -17,13 +16,8 @@ describe 'tictactoe' do
   let(:game_gateway) { InMemoryGameGateway.new(game) }
   let(:view_board) { ViewBoard.new(game_gateway) }
   let(:update_board) { UpdateBoard.new(game_gateway) }
-  let(:clear_board) { ClearBoard.new(game_gateway) }
-  let(:ai_response_board) do
-    AIResponse.new(game_gateway)
-  end
-  let(:evaluate_board) do
-    EvaluateBoard.new(game_gateway)
-  end
+  let(:ai_response_board) { AIResponse.new(game_gateway) }
+  let(:evaluate_board) { EvaluateBoard.new(game_gateway) }
 
   it 'can initialise an empty board' do
     expect(view_board.execute).to eq(
@@ -40,7 +34,7 @@ describe 'tictactoe' do
     )
   end
 
-  it 'can display the winning outcome of any player' do
+  it 'can evaluate to a win for player one, diagonally' do
     update_board.execute('X', at_index: 8)
     update_board.execute('O', at_index: 7)
     update_board.execute('X', at_index: 4)
@@ -49,45 +43,9 @@ describe 'tictactoe' do
 
     response = evaluate_board.execute({})
     expect(response[:outcome]).to eq(:player_one_wins)
-
-    clear_board.execute
-
-    update_board.execute('O', at_index: 8)
-    update_board.execute('X', at_index: 7)
-    update_board.execute('O', at_index: 4)
-    update_board.execute('X', at_index: 6)
-    update_board.execute('O', at_index: 0)
-
-    response = evaluate_board.execute({})
-    expect(response[:outcome]).to eq(:player_two_wins)
-
-    clear_board.execute
-
-    update_board.execute('O', at_index: 3)
-    update_board.execute('X', at_index: 7)
-    update_board.execute('O', at_index: 4)
-    update_board.execute('X', at_index: 6)
-    update_board.execute('O', at_index: 5)
-
-    expect(response[:outcome]).to eq(:player_two_wins)
   end
 
-  it 'can display the outcome of two full games' do
-    update_board.execute('X', at_index: 0)
-    update_board.execute('O', at_index: 1)
-    update_board.execute('O', at_index: 2)
-    update_board.execute('O', at_index: 3)
-    update_board.execute('X', at_index: 4)
-    update_board.execute('X', at_index: 5)
-    update_board.execute('X', at_index: 6)
-    update_board.execute('O', at_index: 7)
-    update_board.execute('O', at_index: 8)
-
-    response = evaluate_board.execute({})
-    expect(response[:outcome]).to eq(:game_over)
-
-    clear_board.execute
-
+  it 'can evaluate to a win for player one' do
     update_board.execute('X', at_index: 0)
     update_board.execute('O', at_index: 3)
     update_board.execute('X', at_index: 6)
@@ -101,6 +59,46 @@ describe 'tictactoe' do
     response = evaluate_board.execute({})
     expect(response[:outcome]).to eq(:player_one_wins)
   end
+  it 'can evaluate to a win for player two' do
+    update_board.execute('X', at_index: 1)
+    update_board.execute('O', at_index: 8)
+    update_board.execute('X', at_index: 7)
+    update_board.execute('O', at_index: 4)
+    update_board.execute('X', at_index: 6)
+    update_board.execute('O', at_index: 0)
+
+    response = evaluate_board.execute({})
+    expect(response[:outcome]).to eq(:player_two_wins)
+  end
+
+  it 'can evaluate to a win for player two' do
+    update_board.execute('X', at_index: 1)
+    update_board.execute('O', at_index: 3)
+    update_board.execute('X', at_index: 7)
+    update_board.execute('O', at_index: 4)
+    update_board.execute('X', at_index: 6)
+    update_board.execute('O', at_index: 5)
+
+    response = evaluate_board.execute({})
+    expect(response[:outcome]).to eq(:player_two_wins)
+  end
+
+  it 'can can evaluate to a draw' do
+    update_board.execute('X', at_index: 0)
+    update_board.execute('O', at_index: 4)
+    update_board.execute('X', at_index: 1)
+    update_board.execute('O', at_index: 2)
+    update_board.execute('X', at_index: 6)
+    update_board.execute('O', at_index: 3)
+    update_board.execute('X', at_index: 5)
+    update_board.execute('O', at_index: 8)
+    update_board.execute('X', at_index: 7)
+
+    response = evaluate_board.execute({})
+    expect(response[:outcome]).to eq(:game_over)
+
+  end
+
 
   it 'can beat the player using AI' do
     def put_players_mark_at(index)
